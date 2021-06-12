61b71fa8afacbd33dbed8b88402eaa4e1b7349d7
LiamMZ <liam.merzhoffmeister@gmail.com>
2021-06-12 22:50:00 +0000

    kinematics class building

--- hardware/motor_driver/include/motor_driver/kinematics.h
+++ hardware/motor_driver/include/motor_driver/kinematics.h
@@ -1,6 +1,3 @@
-// Cosmo
-#include <cosmo/config.h>
-
 //Ros packages
 #include <geometry_msgs/Point.h>
 
@@ -14,10 +11,41 @@ namespace motor_driver
     class Kinematics
     {
     public:
-        Kinematics(cosmo::Configuration config);
-        std::vector<std::vector<double>> four_legs_inverse_kinematics(std::vector<geometry_msgs::Point> leg_poses);
+        /*
+        * Constructor initializes private configuration variables
+        * @param[in] abduction_offset - offset of ad/abduction axes in y from COG
+        * @param[in] l1 - distance to link l1 on robot leg in meters
+        * @param[in] l2 - distance to link l2 on robot leg in meters
+        * @param[in] leg_origins - points of leg origins in the body frame
+        *
+        */
+        Kinematics(const double abduction_offset, const double l1, const double l2, const std::vector<geometry_msgs::Point> leg_origins);
+
+        /*
+        * Function to perform inverse kinematics for each of four passed in foot positions
+        * @param[in] foot_poses - desired foot positions for each of the four legs
+        * @returns - vector of twelve angles corresponding to the three motors for each of the four legs
+        */
+        std::vector< std::vector<double> > four_legs_inverse_kinematics(std::vector<geometry_msgs::Point> foot_poses);
+
     private:
-        std::vector<double> leg_explicit_inverse_kinematics(geometry_msgs::Point r_body_foot, unsigned int leg_index);
-        cosmo::MobilityConfig config_;
+        /*
+        * Function to calculate inverse kinematics on a single foot position
+        * @param[in] r_body_foot - desired position of the foot
+        * @param[in] leg_index - index for given leg
+        */
+        std::vector<double> leg_explicit_inverse_kinematics(const geometry_msgs::Point r_body_foot, const unsigned int leg_index);
+        
+        inline double clamp(const double val, const double low, const double high);
+
+        // distance to link l1 for each of the four legs
+        double LEG_L1;
+        // distance to link l2 for each of the four legs
+        double LEG_L2;
+        // offset from origin to 
+        double ABDUCTION_OFFSET;
+        // offset of ad/abduction axes in y from COG
+        std::vector<geometry_msgs::Point> body_offset;
+
     };
 }//end namespace motor driver
--- hardware/motor_driver/src/kinematics.cpp
+++ hardware/motor_driver/src/kinematics.cpp
@@ -2,25 +2,27 @@
 #include "motor_driver/kinematics.h"
 
 
-motor_driver::Kinematics::Kinematics(cosmo::Configuration config) : config_(config)
-{
-}
+motor_driver::Kinematics::Kinematics(const double abduction_offset, const double l1, const double l2, const std::vector<geometry_msgs::Point> leg_origins)
+ : body_offset(leg_origins), LEG_L1(l1), LEG_L2(l2), ABDUCTION_OFFSET(abduction_offset)
+{}
 
-std::vector<double> motor_driver::Kinematics::leg_explicit_inverse_kinematics(geometry_msgs::Point r_body_foot, unsigned int leg_index)
+std::vector<double> motor_driver::Kinematics::leg_explicit_inverse_kinematics(const geometry_msgs::Point r_body_foot, const unsigned int leg_index)
 {
     double x = r_body_foot.x;
     double y = r_body_foot.y;
     double z = r_body_foot.z;
 
+    // For feet 2 (front left) and 4 (back left), the abduction offset is positive, for the right feet, the abduction offset is negative.
+    double abduction_offset = leg_index==1 || leg_index==3 ? ABDUCTION_OFFSET : -1*ABDUCTION_OFFSET;
+
     // Distance from the body to the foot in the y-z axes in the body frame 
     double R_body_foot_yz = pow(y*y + z*z, 0.5);
     // Distance from ad/abduction axis to foot in the body frame
-    double R_hip_foot_yz = pow((R_body_foot_yz*R_body_foot_yz) - (config_.ABDUCTION_OFFSET*config_.ABDUCTION_OFFSET), 0.5);
+    double R_hip_foot_yz = pow((R_body_foot_yz*R_body_foot_yz) - (abduction_offset*abduction_offset), 0.5);
 
     // Interior angle of the right triangle formed in the y-z plane by the leg that is coincident to the ab/adduction axis
-    // For feet 2 (front left) and 4 (back left), the abduction offset is positive, for the right feet, the abduction offset is negative.
-    double arc_cos_arg = config.ABDUCTION_OFFSET[leg_index]/R_body_foot_yz;
-    arc_cos_arg = std::clamp(arc_cos_arg, -0.99, 0.99);
+    double arc_cos_arg = abduction_offset/R_body_foot_yz;
+    arc_cos_arg = clamp(arc_cos_arg, -0.99, 0.99);
     double phi = acos(arc_cos_arg);
 
     // Angle of the y-z projection of the hip-to-foot vector
@@ -36,18 +38,18 @@ std::vector<double> motor_driver::Kinematics::leg_explicit_inverse_kinematics(ge
     double R_hip_foot = pow(x*x + R_hip_foot_yz*R_hip_foot_yz, 0.5);
     
     // angle between vector going from hip to foot and leg link L1
-    arc_cos_arg = (config_.LEG_L1*config_.LEG_L1 + R_hip_foot*R_hip_foot - config.LEG_L2*config.LEG_L2)/
-                        (2*R_hip_foot*config_.LEG_L1);
-    arc_cos_arg = std::clamp(arc_cos_arg, -0.99, 0.99);
+    arc_cos_arg = (LEG_L1*LEG_L1 + R_hip_foot*R_hip_foot - LEG_L2*LEG_L2)/
+                        (2*R_hip_foot*LEG_L1);
+    arc_cos_arg = clamp(arc_cos_arg, -0.99, 0.99);
     double trident = acos(arc_cos_arg);
     
     // angle of first link relative to tilted negative z axis
     double hip_angle = trident+theta;
 
     // angle between links l1 and l2
-    arc_cos_angle = (config.LEG_L1*config.LEG_L1 + config.LEG_L2*config.LEG_L2 - R_hip_foot*R_hip_foot*)/
-                    (2*config.LEG_L1*config.LEG_L2);
-    arc_cos_arg = std::clamp(arc_cos_arg, -0.99, 0.99);
+    arc_cos_arg = (LEG_L1*LEG_L1 + LEG_L2*LEG_L2 - R_hip_foot*R_hip_foot)/
+                    (2*LEG_L1*LEG_L2);
+    arc_cos_arg = clamp(arc_cos_arg, -0.99, 0.99);
     double beta = acos(arc_cos_arg);
 
     // angle of second link relative to tilted negative z axis
@@ -57,14 +59,13 @@ std::vector<double> motor_driver::Kinematics::leg_explicit_inverse_kinematics(ge
     return ret;
 }
 
-std::vector<std::vector<double>> motor_driver::Kinematics::four_legs_inverse_kinematics(std::vector<geometry_msgs::Point> leg_poses)
+std::vector< std::vector<double> > motor_driver::Kinematics::four_legs_inverse_kinematics(std::vector<geometry_msgs::Point> leg_poses)
 {
-    std::vector<std::vector<double>> alpha;
+    std::vector< std::vector<double> > alpha;
     geometry_msgs::Point body_offset;
     unsigned int leg_index = 0;
     for(geometry_msgs::Point pose : leg_poses)
     {
-        body_offset = config.LEG_ORIGINS[leg_index];
         pose.x = pose.x-body_offset.x;
         pose.y = pose.y-body_offset.y;
         pose.z = pose.z-body_offset.z;
@@ -72,4 +73,11 @@ std::vector<std::vector<double>> motor_driver::Kinematics::four_legs_inverse_kin
         leg_index++;
     }
     return alpha;
+}
+
+double motor_driver::Kinematics::clamp(double value, const double low, const double high)
+{
+    if (value < low) return low;
+    else if (value>high) return high;
+    return value;
 }
